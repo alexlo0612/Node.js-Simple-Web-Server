6ddbbebc940337e5b1dbe9a9e78be00cd35b5bea
alexlo0612 <alexlo0612@hotmail.com.tw>
2018-03-28 16:22:42 +0800

    Beautify

--- Core3/app.js
+++ Core3/app.js
@@ -1,8 +1,8 @@
 //Require Frameworks
-const express = require ('express');
-const bodyParser = require ('body-parser');
-const path = require ('path');
-const request = require ('request');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const request = require('request');
 //Global Variable
 //const apikey = 'haecPfYw9wk6eqUVNGAjumbeIEEFkmIy'
 const apikey = 'hoArfRosT1215'
@@ -16,55 +16,61 @@ let --> block variable
 const app = express();
 
 //Set Up URL Parser
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 //Set up View Engine & its Path
 app.use(express.static(path.join(__dirname, 'css')));
-app.set('view engine','ejs');
-app.set('views',path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 //HTTP Get Request Handling
-app.get('/',function(req,res){
+app.get('/', function(req, res) {
   //res.send('Hello World!')
   console.log('We Got Visit Here!');
-  res.render('index', {error: null});
+  res.render('index', {
+    error: null
+  });
 });
 
 //HTTP Post Request Handling
-app.post('/',function(req,res){
+app.post('/', function(req, res) {
   console.log('Someone Just Post a Request!');
   //console.log(req.body);
   //console.log(req.body.city);
   let city = req.body.city;
   console.log(city);
-  let url =  `http://apidev.accuweather.com/locations/v1/search?q=${city}&apikey=${apikey}`;
-  console.log('API URL is: '+ url);
+  let url = `http://apidev.accuweather.com/locations/v1/search?q=${city}&apikey=${apikey}`;
+  console.log('API URL is: ' + url);
 
-//Make API Call
-request(url, function(err, response, body){
-  if(err){                          //Check Misc. Errors
-    res.render('index',{error:'The Freak"n City You Just Entered Does Not Exist'});
-    console.log('Error!!');
-  } else {
-    //console.log(body);
-    let stage1 = JSON.parse(body); //Parse Json Response
-    //console.log(stage1);
-    if(stage1[0] == undefined){    //Check Validity
-      //res.render('index', {error:'The Freaking City You Just Entered Does Not Exist'});
-      console.log('The City Does Not Exist');
-    } else {                      //Get Location Key + Longitude & Latitude
-      console.log(stage1[0].Key);
-      console.log(stage1[0].GeoPosition.Latitude);
-      console.log(stage1[0].GeoPosition.Longitude);
-      //console.log(stage1[0]);
-      //console.log(stage1.Key);
-      //console.log(stage1.Version);
+  //Make API Call
+  request(url, function(err, response, body) {
+    if (err) { //Check Misc. Errors
+      res.render('index', {
+        error: 'The Freak"n City You Just Entered Does Not Exist'
+      });
+      console.log('Error!!');
+    } else {
+      //console.log(body);
+      let stage1 = JSON.parse(body); //Parse Json Response
+      //console.log(stage1);
+      if (stage1[0] == undefined) { //Check Validity
+        //res.render('index', {error:'The Freaking City You Just Entered Does Not Exist'});
+        console.log('The City Does Not Exist');
+      } else { //Get Location Key + Longitude & Latitude
+        console.log(stage1[0].Key);
+        console.log(stage1[0].GeoPosition.Latitude);
+        console.log(stage1[0].GeoPosition.Longitude);
+        //console.log(stage1[0]);
+        //console.log(stage1.Key);
+        //console.log(stage1.Version);
+      }
     }
-  }
-});
+  });
 });
 
 //Server Starts
-app.listen(3000,function(){
+app.listen(3000, function() {
   console.log('App is listening on port 3000!');
 });
