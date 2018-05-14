c3834348cced135441ee3c30ec3a38cd9c6a6d9f
alexlo0612 <alexlo0612@hotmail.com.tw>
2018-05-14 08:36:06 +0800

    Prettified

--- Core4/app.js
+++ Core4/app.js
@@ -1,56 +1,58 @@
 //Require Frameworks
-const express = require ('express');
-const bodyParser = require ('body-parser');
-const ejs = require ('ejs');
-const qrcode = require ('qrcode-generator');
-const path = require ('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const qrcode = require("qrcode-generator");
+const path = require("path");
 
 //Initialize App
 const app = express();
 
 //Setup URL Parser
-app.use(bodyParser.urlencoded({
-    extended : true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
 //Setup View Engine and Path
-app.use(express.static(path.join(__dirname, 'css')));
-app.set('view engine', 'ejs');
-app.set('views',path.join(__dirname,'views'));
+app.use(express.static(path.join(__dirname, "css")));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 //HTTP GET Request Handling / Routing
-app.get('/',function(req,res){
-    console.log('Visitor Hits');
+app.get("/", (req, res) => {
+  console.log("Visitor Hits");
 
-    res.render('mainpage', {
-        title: 'QR Code Main Page',
-        img: null,
-    });
+  res.render("mainpage", {
+    title: "QR Code Main Page",
+    img: null
+  });
 });
 
 //HTTP POST Request Handling / Routing
-app.post('/', function(req,res){
-    let qrtext = req.body.Textbox;
-    console.log('POST Request Recieved!');
-    console.log(qrtext);
-    //QR Code Module
-    let typeNumber = 0;
-    let errorCorrectionLevel = 'H';
-    let qr = qrcode(typeNumber, errorCorrectionLevel);
-    qr.addData(qrtext);
-    qr.make();
-    let qrcode_img = qr.createImgTag(10,2);
-    //document.getElementById('qr_show').innerHTML = qr.createImgTag(); //-->failed
-    //console.log(qr.createImgTag());
-    console.log('QR Code Generated!');
+app.post("/", (req, res) => {
+  let qrtext = req.body.Textbox;
+  console.log("POST Request Recieved!");
+  console.log(qrtext);
+  //QR Code Module
+  let typeNumber = 0;
+  let errorCorrectionLevel = "H";
+  let qr = qrcode(typeNumber, errorCorrectionLevel);
+  qr.addData(qrtext);
+  qr.make();
+  let qrcode_img = qr.createImgTag(10, 2);
+  //document.getElementById('qr_show').innerHTML = qr.createImgTag(); //-->failed
+  //console.log(qr.createImgTag());
+  console.log("QR Code Generated!");
 
-    res.render('mainpage', {
-        title: 'QR CODE Main Page',
-        img: qrcode_img,
-    });
+  res.render("mainpage", {
+    title: "QR CODE Main Page",
+    img: qrcode_img
+  });
 });
 
 //Start Server
-app.listen(3000 , function(){
-    console.log('Server Listening on Port 3000');
+app.listen(3000, () => {
+  console.log("Server Listening on Port 3000");
 });
