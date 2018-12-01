df007cacfb37e170b2fa81720c0f50661b527835
Algorithm <algorithm@Algorithms-MacBook-Pro.local>
2018-12-01 11:54:05 +0100

    Test

--- Core5/test/create_test.js
+++ Core5/test/create_test.js
@@ -2,7 +2,7 @@
 const assert = require('assert');
 //Import Test Files
 const User = require('../src/user');
-//Start Test (Major Block)
+//Start Test (Major Block) awefweg
 describe('Creating DB Records', () => {
   //Test Subblock
   it('Save User', () => {
