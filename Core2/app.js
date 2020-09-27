1aeaa025de5cdc1a809defc343aadfa1ea19576d
Algorithm <11154774+algo7@users.noreply.github.com>
2020-09-27 19:34:17 +0200

    linter fix for core 2

--- Core2/app.js
+++ Core2/app.js
@@ -1,13 +1,13 @@
 //Require Frameworks
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
-const request = require("request");
-const GoogleMapsAPI = require("googlemaps");
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const request = require('request');
+const GoogleMapsAPI = require('googlemaps');
 //const fs = require ('fs');
 //Global Variable
 //const apikey = 'haecPfYw9wk6eqUVNGAjumbeIEEFkmIy'
-const apikey = "hoArfRosT1215";
+const apikey = 'hoArfRosT1215';
 
 //Define & Initialize App
 const app = express();
@@ -16,7 +16,7 @@ const app = express();
 const (var) --> global variable
 let --> block variable
 */
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   (res.locals.errors = null),
     (res.locals.Latitude = null),
     (res.locals.Longitude = null),
@@ -29,32 +29,32 @@ app.use(function(req, res, next) {
 //Set Up URL Parser
 app.use(
   bodyParser.urlencoded({
-    extended: true
+    extended: true,
   })
 );
 
 //Set up View Engine & its Path
-app.use(express.static(path.join(__dirname, "css")));
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, 'css')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 //HTTP Get Request Handling
-app.get("/", function(req, res) {
+app.get('/', function (req, res) {
   //res.send('Hello World!')
-  console.log("We Got Visit Here!");
-  res.render("index", {
+  console.log('We Got Visit Here!');
+  res.render('index', {
     error: null,
-    Latitude: "Latitude: ",
-    Longitude: "Longitude: ",
-    Weather: "Weather: ",
-    Temperature: "Temperature: ",
-    place_id: "ChIJrUldGqrWaDQR2vYXX6n7SBM"
+    Latitude: 'Latitude: ',
+    Longitude: 'Longitude: ',
+    Weather: 'Weather: ',
+    Temperature: 'Temperature: ',
+    place_id: 'ChIJrUldGqrWaDQR2vYXX6n7SBM',
   });
 });
 
 //HTTP Post Request Handling
-app.post("/", function(req, res) {
-  console.log("Someone Just Post a Request!");
+app.post('/', function (req, res) {
+  console.log('Someone Just Post a Request!');
   //console.log(req.body);
   //console.log(req.body.city);
   let city = req.body.city;
@@ -62,22 +62,22 @@ app.post("/", function(req, res) {
   //let url = `https://dataservice.accuweather.com/locations/v1/search?q=${city}&apikey=${apikey}`;
   //let url = `http://api.accuweather.com/locations/v1/search?q=${city}&apikey=${apikey}`;
   let url = `https://apidev.accuweather.com/locations/v1/search?q=${city}&apikey=${apikey}`;
-  console.log("API URL is: " + url);
-  console.log("Encoded API URL is: " + encodeURI(url));
+  console.log('API URL is: ' + url);
+  console.log('Encoded API URL is: ' + encodeURI(url));
 
   //Make API Call
-  request(encodeURI(url), function(err, response, body) {
+  request(encodeURI(url), function (err, response, body) {
     if (err) {
       //Check Misc. Errors
-      res.render("index", {
-        error: "Error!!",
+      res.render('index', {
+        error: 'Error!!',
         Latitude: null,
         Longitude: null,
         Weather: null,
         Temperature: null,
-        place_id: null
+        place_id: null,
       });
-      console.log("URLError!!");
+      console.log('URLError!!');
     } else {
       //console.log(encodeURI(url));
       //console.log(body);
@@ -86,15 +86,15 @@ app.post("/", function(req, res) {
       //console.log(stage1);
       if (stage1[0] == undefined) {
         //Check Validity
-        res.render("index", {
-          error: "The Freaking City You Just Entered Does Not Exist",
+        res.render('index', {
+          error: 'The Freaking City You Just Entered Does Not Exist',
           Latitude: null,
           Longitude: null,
           Weather: null,
           Temperature: null,
-          place_id: null
+          place_id: null,
         });
-        console.log("The City Does Not Exist");
+        console.log('The City Does Not Exist');
       } else {
         //Get Location Key + Longitude & Latitude
         //console.log(stage1[0].Key);
@@ -114,17 +114,17 @@ app.post("/", function(req, res) {
         //let url2 = `http://dataservice.accuweather.com/currentconditions/v1/${locationkey}.json?language=en&apikey=${apikey}`;
         //let url2 = `http://api.accuweather.com/currentconditions/v1/${locationkey}.json?language=en&apikey=${apikey}`;
         let url2 = `https://apidev.accuweather.com/currentconditions/v1/${locationkey}.json?language=en&apikey=${apikey}`;
-        request(encodeURI(url2), function(err, response, body) {
+        request(encodeURI(url2), function (err, response, body) {
           if (err) {
-            res.render("index", {
-              error: "Error!!!",
+            res.render('index', {
+              error: 'Error!!!',
               Latitude: null,
               Longitude: null,
               Weather: null,
               Temperature: null,
-              place_id: null
+              place_id: null,
             });
-            console.log("Error!!");
+            console.log('Error!!');
           } else {
             let stage2 = JSON.parse(body);
             let Weather = stage2[0].WeatherText;
@@ -133,56 +133,56 @@ app.post("/", function(req, res) {
             console.log(stage2[0].Temperature.Metric.Value);
             //google map
             const googleconfig = {
-              key: "AIzaSyCkkjZo3OlxKIaMpBSv2b2goXA9Vuru4rs",
+              key: 'AIzaSyCkkjZo3OlxKIaMpBSv2b2goXA9Vuru4rs',
               stagger_time: 1000,
               encode_polylines: true,
-              secure: true
+              secure: true,
             };
             const gmAPI = new GoogleMapsAPI(googleconfig);
 
             const reverseGeoCodeParams = {
               latlng: `${Latitude},${Longitude}`,
-              result_type: "postal_code",
-              language: "en",
-              location_type: "APPROXIMATE"
+              result_type: 'postal_code',
+              language: 'en',
+              location_type: 'APPROXIMATE',
             };
-            gmAPI.reverseGeocode(reverseGeoCodeParams, function(err, result) {
+            gmAPI.reverseGeocode(reverseGeoCodeParams, function (err, result) {
               if (err) {
-                res.render("index", {
-                  error: "Error!!!",
+                res.render('index', {
+                  error: 'Error!!!',
                   Latitude: null,
                   Longitude: null,
                   Weather: null,
                   Temperature: null,
-                  place_id: null
+                  place_id: null,
                 });
-                console.log("Error!!");
+                console.log('Error!!');
               } else {
                 //console.log(result)
                 //let place_id = result.results[0].place_id;
                 console.log(result.status);
-                if (result.status !== "OK") {
-                  res.render("index", {
-                    error: "Map Error!!!",
+                if (result.status !== 'OK') {
+                  res.render('index', {
+                    error: 'Map Error!!!',
                     Latitude: null,
                     Longitude: null,
                     Weather: null,
                     Temperature: null,
-                    place_id: null
+                    place_id: null,
                   });
-                  console.log("Map Error!!");
+                  console.log('Map Error!!');
                 } else {
                   let place_id = result.results[0].place_id;
                   console.log(place_id);
                   //console.log(result.results[0].place_id);
                   //console.log(result.results[0].place_id);
-                  res.render("index", {
+                  res.render('index', {
                     error: null,
-                    Latitude: "Latitude: " + Latitude,
-                    Longitude: "Longitude: " + Longitude,
-                    Weather: "Condition: " + Weather,
-                    Temperature: "Temperature: " + Temperature + " Celsius",
-                    place_id: place_id
+                    Latitude: 'Latitude: ' + Latitude,
+                    Longitude: 'Longitude: ' + Longitude,
+                    Weather: 'Condition: ' + Weather,
+                    Temperature: 'Temperature: ' + Temperature + ' Celsius',
+                    place_id: place_id,
                     //Map end
                   });
                 }
@@ -214,6 +214,6 @@ app.post("/", function(req, res) {
 });
 
 //Server Starts
-app.listen(3000, function() {
-  console.log("App is listening on port 3000!");
+app.listen(3000, function () {
+  console.log('App is listening on port 3000!');
 });
