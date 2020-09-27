74473f41790b261c70888df8a68c8b7b9f1b1616
Algorithm <11154774+algo7@users.noreply.github.com>
2020-09-27 19:33:28 +0200

    delete defunc repo

---
+++ Core2/app.js
@@ -0,0 +1,219 @@
+//Require Frameworks
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const request = require("request");
+const GoogleMapsAPI = require("googlemaps");
+//const fs = require ('fs');
+//Global Variable
+//const apikey = 'haecPfYw9wk6eqUVNGAjumbeIEEFkmIy'
+const apikey = "hoArfRosT1215";
+
+//Define & Initialize App
+const app = express();
+
+/*
+const (var) --> global variable
+let --> block variable
+*/
+app.use(function(req, res, next) {
+  (res.locals.errors = null),
+    (res.locals.Latitude = null),
+    (res.locals.Longitude = null),
+    (res.locals.Weather = null),
+    (res.locals.Temperature = null),
+    (res.locals.place_id = null);
+  next();
+});
+
+//Set Up URL Parser
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+//Set up View Engine & its Path
+app.use(express.static(path.join(__dirname, "css")));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+//HTTP Get Request Handling
+app.get("/", function(req, res) {
+  //res.send('Hello World!')
+  console.log("We Got Visit Here!");
+  res.render("index", {
+    error: null,
+    Latitude: "Latitude: ",
+    Longitude: "Longitude: ",
+    Weather: "Weather: ",
+    Temperature: "Temperature: ",
+    place_id: "ChIJrUldGqrWaDQR2vYXX6n7SBM"
+  });
+});
+
+//HTTP Post Request Handling
+app.post("/", function(req, res) {
+  console.log("Someone Just Post a Request!");
+  //console.log(req.body);
+  //console.log(req.body.city);
+  let city = req.body.city;
+  console.log(city);
+  //let url = `https://dataservice.accuweather.com/locations/v1/search?q=${city}&apikey=${apikey}`;
+  //let url = `http://api.accuweather.com/locations/v1/search?q=${city}&apikey=${apikey}`;
+  let url = `https://apidev.accuweather.com/locations/v1/search?q=${city}&apikey=${apikey}`;
+  console.log("API URL is: " + url);
+  console.log("Encoded API URL is: " + encodeURI(url));
+
+  //Make API Call
+  request(encodeURI(url), function(err, response, body) {
+    if (err) {
+      //Check Misc. Errors
+      res.render("index", {
+        error: "Error!!",
+        Latitude: null,
+        Longitude: null,
+        Weather: null,
+        Temperature: null,
+        place_id: null
+      });
+      console.log("URLError!!");
+    } else {
+      //console.log(encodeURI(url));
+      //console.log(body);
+      //console.log('Here:' + body)
+      let stage1 = JSON.parse(body); //Parse Json Response
+      //console.log(stage1);
+      if (stage1[0] == undefined) {
+        //Check Validity
+        res.render("index", {
+          error: "The Freaking City You Just Entered Does Not Exist",
+          Latitude: null,
+          Longitude: null,
+          Weather: null,
+          Temperature: null,
+          place_id: null
+        });
+        console.log("The City Does Not Exist");
+      } else {
+        //Get Location Key + Longitude & Latitude
+        //console.log(stage1[0].Key);
+        let locationkey = stage1[0].Key;
+        let Latitude = stage1[0].GeoPosition.Latitude;
+        let Longitude = stage1[0].GeoPosition.Longitude;
+        console.log(stage1[0].GeoPosition.Latitude);
+        console.log(stage1[0].GeoPosition.Longitude);
+
+        /*res.render('index', {
+          error: null,
+          Latitude: 'Latitude: ' + Latitude,
+          Longitude: 'Longitude: ' + Longitude,
+          Weather: null,
+          Temperature: null
+        }); */
+        //let url2 = `http://dataservice.accuweather.com/currentconditions/v1/${locationkey}.json?language=en&apikey=${apikey}`;
+        //let url2 = `http://api.accuweather.com/currentconditions/v1/${locationkey}.json?language=en&apikey=${apikey}`;
+        let url2 = `https://apidev.accuweather.com/currentconditions/v1/${locationkey}.json?language=en&apikey=${apikey}`;
+        request(encodeURI(url2), function(err, response, body) {
+          if (err) {
+            res.render("index", {
+              error: "Error!!!",
+              Latitude: null,
+              Longitude: null,
+              Weather: null,
+              Temperature: null,
+              place_id: null
+            });
+            console.log("Error!!");
+          } else {
+            let stage2 = JSON.parse(body);
+            let Weather = stage2[0].WeatherText;
+            let Temperature = stage2[0].Temperature.Metric.Value;
+            console.log(stage2[0].WeatherText);
+            console.log(stage2[0].Temperature.Metric.Value);
+            //google map
+            const googleconfig = {
+              key: "AIzaSyCkkjZo3OlxKIaMpBSv2b2goXA9Vuru4rs",
+              stagger_time: 1000,
+              encode_polylines: true,
+              secure: true
+            };
+            const gmAPI = new GoogleMapsAPI(googleconfig);
+
+            const reverseGeoCodeParams = {
+              latlng: `${Latitude},${Longitude}`,
+              result_type: "postal_code",
+              language: "en",
+              location_type: "APPROXIMATE"
+            };
+            gmAPI.reverseGeocode(reverseGeoCodeParams, function(err, result) {
+              if (err) {
+                res.render("index", {
+                  error: "Error!!!",
+                  Latitude: null,
+                  Longitude: null,
+                  Weather: null,
+                  Temperature: null,
+                  place_id: null
+                });
+                console.log("Error!!");
+              } else {
+                //console.log(result)
+                //let place_id = result.results[0].place_id;
+                console.log(result.status);
+                if (result.status !== "OK") {
+                  res.render("index", {
+                    error: "Map Error!!!",
+                    Latitude: null,
+                    Longitude: null,
+                    Weather: null,
+                    Temperature: null,
+                    place_id: null
+                  });
+                  console.log("Map Error!!");
+                } else {
+                  let place_id = result.results[0].place_id;
+                  console.log(place_id);
+                  //console.log(result.results[0].place_id);
+                  //console.log(result.results[0].place_id);
+                  res.render("index", {
+                    error: null,
+                    Latitude: "Latitude: " + Latitude,
+                    Longitude: "Longitude: " + Longitude,
+                    Weather: "Condition: " + Weather,
+                    Temperature: "Temperature: " + Temperature + " Celsius",
+                    place_id: place_id
+                    //Map end
+                  });
+                }
+              }
+            });
+
+            /*console.log(body);
+            console.log(stage2);
+            let Weather = stage2[0].WeatherText;
+            let Temp = stage2[0].Temperature.Metric;
+            console.log(Weather + Temp); */
+
+            /*function parsed(body){
+              if(body && body.length > 0){
+                let condition = body[0];
+                let temp = condition.Temperature.Metric;
+                console.log(temp); */
+            //console.log(body[0]);
+            //let Tempp = JSON.parse(stage2[0].Temperature);
+            //console.log(Tempp[0]);
+          }
+        });
+        //console.log(stage1[0]);
+        //console.log(stage1.Key);
+        //console.log(stage1.Version);
+      }
+    }
+  });
+});
+
+//Server Starts
+app.listen(3000, function() {
+  console.log("App is listening on port 3000!");
+});
--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Core: Node.js-Simple-Web-Server
+
 A basic HTTP web server built with Node.js
-# Core2: Node.js-MongoDB-Web-App
-A basic web app built with Node.js that interacts with MongoDB
-# Core3: Node.js-Weather-Site
+
+# Core2: Node.js-Weather-Site
+
 An interactive site that makes calls to AccuWeather API. The site will retrieve the weather information of the given city
 
 Live at: https://app.hashx.tk
-# Core4: Node.js-QR-Code-Generator
-An interactive site that generates QR Code from user inputs
 
-# Contribution
-[![](https://sourcerer.io/fame/alexlo0612/alexlo0612/Node.js-Web-App/images/0)](https://sourcerer.io/fame/alexlo0612/alexlo0612/Node.js-Web-App/links/0)[![](https://sourcerer.io/fame/alexlo0612/alexlo0612/Node.js-Web-App/images/1)](https://sourcerer.io/fame/alexlo0612/alexlo0612/Node.js-Web-App/links/1)[![](https://sourcerer.io/fame/alexlo0612/alexlo0612/Node.js-Web-App/images/2)](https://sourcerer.io/fame/alexlo0612/alexlo0612/Node.js-Web-App/links/2)[![](https://sourcerer.io/fame/alexlo0612/alexlo0612/Node.js-Web-App/images/3)](https://sourcerer.io/fame/alexlo0612/alexlo0612/Node.js-Web-App/links/3)[![](https://sourcerer.io/fame/alexlo0612/alexlo0612/Node.js-Web-App/images/4)](https://sourcerer.io/fame/alexlo0612/alexlo0612/Node.js-Web-App/links/4)[![](https://sourcerer.io/fame/alexlo0612/alexlo0612/Node.js-Web-App/images/5)](https://sourcerer.io/fame/alexlo0612/alexlo0612/Node.js-Web-App/links/5)[![](https://sourcerer.io/fame/alexlo0612/alexlo0612/Node.js-Web-App/images/6)](https://sourcerer.io/fame/alexlo0612/alexlo0612/Node.js-Web-App/links/6)[![](https://sourcerer.io/fame/alexlo0612/alexlo0612/Node.js-Web-App/images/7)](https://sourcerer.io/fame/alexlo0612/alexlo0612/Node.js-Web-App/links/7)
+# Core3: Node.js-QR-Code-Generator
+
+An interactive site that generates QR Code from user inputs
